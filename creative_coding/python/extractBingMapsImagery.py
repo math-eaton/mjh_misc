a1ff5b854c6e511c25ef6305d70ab964f446cdb1
math-eaton <mheaton@ciesin.columbia.edu>
2023-08-04 14:44:24 -0400

    use center point instead of area

--- creative_coding/python/extractBingMapsImagery.py
+++ creative_coding/python/extractBingMapsImagery.py
@@ -1,9 +1,11 @@
 import requests
 import os
 import config
+import pandas as pd
+
 
 # map config
-zoom_level = 4
+zoom_level = 15
 map_size = "500,500"
 map_style = "Aerial"
 output_dir = "output/bing_imagery"
@@ -15,14 +17,15 @@ def get_bing_map_image(center_latitude, center_longitude):
 
     # Define the parameters for the API request
     params = {
-        "mapArea": f"{center_latitude-0.01},{center_longitude-0.01},{center_latitude+0.01},{center_longitude+0.01}",
+        "center": f"{center_latitude},{center_longitude}",
+        "zoomlevel": zoom_level,
         "mapSize": map_size,
         "format": "jpeg",
         "key": config.bing_api_key,
     }
 
     # Create the full URL for the API request
-    full_url = base_url + map_style + "?" + "&".join(f"{key}={value}" for key, value in params.items())
+    full_url = base_url + map_style + "/" + params["center"] + "/" + str(params["zoomlevel"]) + "?mapSize=" + params["mapSize"] + "&format=" + params["format"] + "&key=" + params["key"]
 
     # Make the API request
     response = requests.get(full_url)
@@ -38,6 +41,29 @@ def get_bing_map_image(center_latitude, center_longitude):
     else:
         print(f"Failed to get map image: {response.content}")
 
-# Example usage
-get_bing_map_image(47.6097, -122.3331)
+# Read the CSV file
+df = pd.read_csv('creative_coding/data/transmitter_subset.csv')
+
+# Loop over each row in the DataFrame
+for _, row in df.iterrows():
+    # Get the coordinates from the 'transmitter_site' column
+    # Assuming the column contains strings like '47.6097,-122.3331'
+    center_latitude, center_longitude = map(float, row['transmitter_site'].split(','))
+
+    # Download the image for these coordinates
+    get_bing_map_image(center_latitude, center_longitude)
+
+    # Loop over each row in the DataFrame
+# for i, row in df.iterrows():
+#     # Get the coordinates from the 'transmitter_site' column
+#     # Assuming the column contains strings like '47.6097,-122.3331'
+#     center_latitude, center_longitude = map(float, row['transmitter_site'].split(','))
+
+#     # Download the image for these coordinates
+#     get_bing_map_image(center_latitude, center_longitude)
+
+#     # Print a message
+#     print(f"Downloaded image {i+1} for coordinates ({center_latitude}, {center_longitude})")
+
+
 print("done.")
