4ccde4d8b084606b5a1b92ce7d53b364b8f01803
math-eaton <mheaton@ciesin.columbia.edu>
2023-08-08 18:15:22 -0400

    fix looping behavior

--- video/compositeGifs.py
+++ video/compositeGifs.py
@@ -1,5 +1,5 @@
-from PIL import Image
 import imageio
+import numpy as np
 from tqdm import tqdm
 
 def superimpose_gifs(gif_path1, gif_path2, output_path):
@@ -14,27 +14,34 @@ def superimpose_gifs(gif_path1, gif_path2, output_path):
     # Process each frame
     output_frames = []
     for frame1, frame2 in tqdm(zip(reader1, reader2), total=reader1.get_length(), desc="Processing frames"):
-        # Convert to PIL Image
-        pil_frame1 = Image.fromarray(frame1)
-        pil_frame2 = Image.fromarray(frame2)
+        # Ensure frames have an alpha channel
+        if frame1.shape[2] == 3:
+            frame1 = np.concatenate([frame1, np.full_like(frame1[..., :1], 255)], axis=2)
+        if frame2.shape[2] == 3:
+            frame2 = np.concatenate([frame2, np.full_like(frame2[..., :1], 255)], axis=2)
 
-        # Make sure both images are in RGBA mode
-        pil_frame1 = pil_frame1.convert("RGBA")
-        pil_frame2 = pil_frame2.convert("RGBA")
+        # Normalize the alpha channel
+        alpha1 = frame1[:, :, 3] / 255.0
+        alpha2 = frame2[:, :, 3] / 255.0
 
-        # Composite images
-        composite_frame = Image.alpha_composite(pil_frame1, pil_frame2)
+        # Composite the RGB channels
+        composite_rgb = alpha1[..., None] * frame1[:, :, :3] + (1 - alpha1[..., None]) * alpha2[..., None] * frame2[:, :, :3]
+
+        # Combine the alpha channels
+        composite_alpha = (alpha1 + (1 - alpha1) * alpha2) * 255
+
+        # Create the final frame
+        composite_frame = np.concatenate([composite_rgb, composite_alpha[..., None]], axis=2).astype(np.uint8)
 
         # Append to output frames
         output_frames.append(composite_frame)
 
     # Save the output GIF
-    imageio.mimsave(output_path, [frame.convert("RGBA") for frame in output_frames], 'GIF', duration=0.09, loop=0, disposal=2)
+    imageio.mimsave(output_path, output_frames, format='GIF', duration=0.09, loop=0, disposal=2)
 
     print("done.")
 
-
 gif_path1 = '/Users/matthewheaton/Documents/GitHub/imagery_scraper/output/animations/area_inverted.gif'
-gif_path2 = '/Users/matthewheaton/Documents/GitHub/imagery_scraper/output/animations/polyline_images_inverted.gif'
-output_path = '/Users/matthewheaton/Documents/GitHub/imagery_scraper/output/animations/area_combined_inverted.gif'
+gif_path2 = '/Users/matthewheaton/Documents/GitHub/imagery_scraper/output/animations/polyline_inverted.gif'
+output_path = '/Users/matthewheaton/Documents/GitHub/imagery_scraper/output/animations/combined_inverted.gif'
 superimpose_gifs(gif_path1, gif_path2, output_path)
