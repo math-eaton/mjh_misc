d33f96a083571ea6d86a9c6b289bddd6ac58d3e2
math-eaton <mheaton@ciesin.columbia.edu>
2023-10-30 02:47:11 -0400

    rm secret

--- creative_coding/isolines_3D/secret.js
+++
@@ -1,8 +0,0 @@
-// secret.js
-var MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoibWpoMjI0MSIsImEiOiJjbDZhNWdtdWcwemYwM2Nyejg4azR6MjdtIn0.jcFs6eofGMkSv7Gokq_b6A';
-
-export default MAPBOX_ACCESS_TOKEN;
-
-// var GOOGLE_MAPS_KEY = 'AIzaSyAJb9s5EZOr7v68BHE0wfKbyZsLukvbvOc';
-
-// export default GOOGLE_MAPS_KEY;
